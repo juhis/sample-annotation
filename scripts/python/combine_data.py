13a26f89a19f6ca4d108ea76b65bd1458c57d8c5
unknown <FolkersmaP@15-000-0669.zkh.umcg.intra>
2015-10-09 14:46:38 +0200

    added comments

--- scripts/python/combine_data.py
+++ scripts/python/combine_data.py
@@ -1,18 +1,15 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      FolkersmaP
-#
+# Name:         combine_data.py
+# Purpose:      This script combines GSMAnnotation.txt with 2015_09_24_ENA.txt
+#               and 2015_09_24_ENA_with_SRA_ArrayExpress_filtered_columns.txt
+# Author:      Pytrik Folkertsma
 # Created:     05-10-2015
-# Copyright:   (c) FolkersmaP 2015
-# Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
 
-def combineData(firstLine, gsmAnnotations):
+def combine_ENA_with_GEO(firstLine, gsmAnnotations):
     '''
-    Combine 2015_09_24_ENA.txt with GSMAnnotation.txt.
+    Combines 2015_09_24_ENA.txt with GSMAnnotation.txt.
     '''
     newFile = open('2015_09_24_ENA_with_GEO_filtered_columns.txt', 'w')
     oldFile = open('2015_09_24_ENA.txt', 'r')
@@ -27,13 +24,13 @@ def combineData(firstLine, gsmAnnotations):
             newFile.write('\t'.join(line) + ('\t' * 10) + '\n')
     newFile.close()
 
-def combineAllData(firstLine, gsmAnnotations):
+def combine_ENA_ArrayExpress_with_GEO(firstLine, gsmAnnotations):
     '''
-    Combine 2015_09_24_ENA_with_SRA_ArrayExpress_filtered_columns.txt with GSMAnnotation.txt
+    Combines 2015_09_24_ENA_with_SRA_ArrayExpress_filtered_columns.txt with GSMAnnotation.txt
     '''
     count = 0
-    newFile = open('2015_09_24_ENA_with_SRA_ArrayExpress_GEO_filtered_columns.txt', 'w')
-    oldFile = open('2015_09_24_ENA_with_SRA_ArrayExpress_filtered_columns.txt', 'r')
+    newFile = open('../../data/2015_09_24_ENA_with_SRA_ArrayExpress_GEO_filtered_columns.txt', 'w')
+    oldFile = open('../../data/2015_09_24_ENA_with_SRA_ArrayExpress_filtered_columns.txt', 'r')
     newFile.write(oldFile.readline().strip())
     newFile.write('\tGEO_' + '\tGEO_'.join(firstLine[1::]).upper() + '\n')
     for line in oldFile:
@@ -48,13 +45,13 @@ def combineAllData(firstLine, gsmAnnotations):
 
 def main():
     gsmAnnotations = dict()
-    data = open('GSMAnnotation.txt', 'r', encoding="utf8")
+    data = open('../../data/GSMAnnotation.txt', 'r', encoding="utf8")
     firstLine = data.readline().replace('\n', '').split('\t')
     for line in data:
         line = line.strip().split('\t')
         gsmAnnotations[line[0]] = line[1::]
-    combineData(firstLine, gsmAnnotations) #Combine 2015_09_24_ENA.txt with the GSMAnnotation.
-    combineAllData(firstLine, gsmAnnotations) #Combine 2015_09_24_ENA_with_SRA_ArrayExpress_filtered_columns with the GSMAnnotation.
+    #combine_ENA_with_GEO(firstLine, gsmAnnotations) #Combine 2015_09_24_ENA.txt with the GSMAnnotation.
+    combine_ENA_ArrayExpress_with_GEO(firstLine, gsmAnnotations) #Combine 2015_09_24_ENA_with_SRA_ArrayExpress_filtered_columns with the GSMAnnotation.
 
 if __name__ == '__main__':
     main()
